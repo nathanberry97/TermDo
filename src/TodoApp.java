920d3fa5915b1dbb59beaa00f913cdd4fa2d92fa
nathanberry97 <nathanberry97@live.com>
2022-04-02 13:48:43 +0100

    updates to ui

--- src/TodoApp.java
+++ src/TodoApp.java
@@ -11,19 +11,19 @@ class TodoApp{
 		Scanner userInput = new Scanner(System.in);
 		
 		while (running){
-			ui.clearScreen();
-			System.out.println("Display your different todo lists? (y/n)");
-			String decision = userInput.nextLine();
+			String decision = ui.userOptions(userInput);
 
 			if(decision.equals("y")){
-				ui.clearScreen();
-				ui.projectOutput();
-				userInput.nextLine();
+				ui.displayTodoList(userInput);
 			}
-			else{
-				if(decision.equals("n")){
-					running = false;
-					ui.clearScreen();
+			else{ 
+				if (decision.equals("i")){
+					ui.insertTodoList(userInput);
+				}
+				else{
+					if(decision.equals("n")){
+						running = ui.quitTodo();
+					}
 				}
 			}
 		}
@@ -41,6 +41,32 @@ class TodoApp{
 	public void clearScreen(){
 		System.out.println("\033[H\033[2J");
 		System.out.flush();
+	}
+
+	public String userOptions(Scanner userInput){
+		ui.clearScreen();
+		System.out.println("Display todo lists? (y/n)");
+		System.out.println("Insert new todo list (i)");
+		String option = userInput.nextLine();
+		return(option);
+	}
+
+	public void displayTodoList(Scanner userInput){
+		ui.clearScreen();
+		ui.projectOutput();
+		userInput.nextLine();
+	}
+
+	public void insertTodoList(Scanner userInput){
+		ui.clearScreen();
+		System.out.println("Please enter todo list name:");
+		String newProject = userInput.nextLine();
+		db.putProjects(conn, newProject);
+	}
+	
+	public boolean quitTodo(){
+		ui.clearScreen();
+		return(false);
 	}
 
 }
@@ -75,7 +101,41 @@ class Database{
 		catch(Exception e){
 			e.printStackTrace();
 		}
+
 		return(projectList);
 	}
 
+	public void putProjects(Connection conn, String newProject){
+		String query = "INSERT INTO project(VALUES(?, ?));";	
+		int id = getProjectId(conn);
+
+		try{
+			PreparedStatement st = conn.prepareStatement(query);
+			st.setInt(1, id);
+			st.setString(2, newProject);	
+
+			st.executeUpdate();
+		}
+		catch(SQLException e){
+			e.printStackTrace();
+		}
+	}
+	
+	private int getProjectId(Connection conn) {
+		String query = "SELECT MAX(project_id) FROM project";
+		int maxid = -1;
+
+		try{
+			PreparedStatement st = conn.prepareStatement(query);
+			ResultSet rs = st.executeQuery();
+			while(rs.next()){
+				maxid = rs.getInt(1);
+			}
+		}
+		catch (Exception e){
+			e.printStackTrace();
+		}
+
+		return(maxid + 1);
+	}
 }
